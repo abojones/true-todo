5a60fe30de23e023af904b67aab52f47f2c1809f
Abokina Wilson-Jones <abo_jones@hotmail.co.uk>
2020-04-03 01:16:17 +0100

    App component records added items

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,10 @@ export default class App extends Component {
     super(props);
     this.state = {
       activeList: [],
-      pendingList: []
+      pendingList: [],
+      item: "",
+      description: "",
+      recArmed: false,
     }
   }
 
@@ -21,9 +24,67 @@ export default class App extends Component {
     }
   }
 
+  handleChange = (e) => {
+    const target = e.target, name = target.name, value = target.value;
+    this.setState({
+      [name]: value
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let pendingList = this.state.pendingList.slice();
+    let item = {
+      name: this.state.item,
+      description: this.state.description,
+    };
+    pendingList.push(item);
+    this.setState({
+      pendingList,
+      item: "",
+      description: ""
+    })
+  };
+
+  handleRecord = () => {
+    const recArmed = !this.state.recArmed;
+    this.setState({
+      recArmed,
+      item: "",
+      description: ""
+    })
+  }
+
   render() {
     return (
       <div data-test="component-app">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="item">
+            item:
+            <input disabled={!this.state.recArmed}
+                   data-test="name-input"
+                   onChange={this.handleChange}
+                   type="text" name="item"
+                   value={this.state.item}
+            />
+          </label>
+          <label htmlFor="description">
+            description:
+            <input disabled={!this.state.recArmed}
+                   data-test="desc-input"
+                   onChange={this.handleChange}
+                   type="text"
+                   name="description"
+                   value={this.state.description}
+            />
+          </label>
+          <button data-test-id="submit-btn"
+                  disabled={this.state.item === "" || this.state.description === ""}
+                  type="submit">Submit</button>
+        </form>
+        <button data-test="rec-ctrl" onClick={this.handleRecord}>
+          {this.state.recArmed ? "Stop recording" : "Start recording"}
+        </button>
         <List activeList={this.state.activeList} />
       </div>
     );
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import App from './App';
 import { shallow } from 'enzyme';
+import {findByTestAttr} from "../../test/testUtils";
+
+const setup = (state=null) => {
+  let wrapper = shallow(<App/>);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
 
 test('renders without error', () => {
   const wrapper = shallow(<App/>);
   const component = wrapper.find('[data-test="component-app"]');
   expect(component.length).toBe(1);
+});
+
+describe('Component not Recording', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({recArmed: false})
+  });
+  test('Inputs are disabled', () => {
+    const nameInput = findByTestAttr(wrapper, 'name-input'),
+      descInput = findByTestAttr(wrapper, 'desc-input');
+    expect(nameInput.prop('disabled') && descInput.prop('disabled')).toBe(true);
+  });
+  test('Start recording button is enabled', () => {
+    const recordBtn = findByTestAttr(wrapper, 'rec-ctrl');
+    expect(recordBtn.text().toLowerCase()).toContain("start")
+  });
+});
+
+describe('Component is recording', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({recArmed: true})
+  });
+  test('Inputs are enabled', () => {
+    const nameInput = findByTestAttr(wrapper, 'name-input'),
+      descInput = findByTestAttr(wrapper, 'desc-input');
+    expect(nameInput.prop('disabled') && descInput.prop('disabled')).toBe(false);
+  });
+  test('Stop recording Button is enabled', () => {
+    const recordBtn = findByTestAttr(wrapper, 'rec-ctrl');
+    expect(recordBtn.text().toLowerCase()).toContain("stop")
+  });
 });
